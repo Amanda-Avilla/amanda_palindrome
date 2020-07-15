0976d61d83930a1e8760446a5b63a4fba0d37e34
Amanda-Avilla <amanda@pantydrop.me>
2020-07-15 11:56:02 -0700

    fixed rakefile

--- lib/amanda_palindrome.rb
+++ lib/amanda_palindrome.rb
@@ -2,5 +2,19 @@ require "amanda_palindrome/version"
 
 module AmandaPalindrome
   class Error < StandardError; end
-  # Your code goes here...
+  def test_non_palindrome
+    assert !"apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    skip
+  end
+
+  def test_palindrome_with_punctuation
+    skip
+  end
 end
