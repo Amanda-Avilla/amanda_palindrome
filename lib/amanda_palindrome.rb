91627089b2ad0526d4d8f495157b8d523a3bc237
Amanda-Avilla <amanda@pantydrop.me>
2020-07-15 13:44:57 -0700

    Finish working and refactored palindrome method

--- lib/amanda_palindrome.rb
+++ lib/amanda_palindrome.rb
@@ -2,12 +2,15 @@ require "amanda_palindrome/version"
 
 class String
 
+  # Returns true for a palindrome, false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
   end
-private
 
-  def processed_content
-    self.downcase
-  end
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
--- test/amanda_palindrome_test.rb
+++ test/amanda_palindrome_test.rb
@@ -2,10 +2,17 @@ require "test_helper"
 
 class AmandaPalindromeTest < Minitest::Test
   def test_non_palindrome
-    assert !"apple".palindrome?
+    refute "apple".palindrome?
   end
 
   def test_literal_palindrome
     assert "racecar".palindrome?
   end
+  def test_mixed_case_palindrome
+  assert "RaceCar".palindrome?
+end
+
+def test_palindrome_with_punctuation
+  assert "Madam, I'm Adam.".palindrome?
+end
 end
